193b4959aead7d39583ea93dfe0a6bb38b899849
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-11-18 16:24:31 +0200

    v1

--- index.js
+++ index.js
@@ -9,13 +9,9 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
-    this.cars = [];
-    /**
-     *
-     * @param {Person} Person
-     */
     this.outputInformationAboutPerson = function (Person) {
-        console.log(this.name, this.age)
+        console.log(`Name: ${this.name}, Age:  ${this.age}`)
+        console.log('/-------------------------------------/')
     }
 }
 
@@ -24,7 +20,6 @@ function Person(name, age) {
  * @param {string} model
  * @param {number} year
  * @param {string} number
- * @param {Person} owner
  * @constructor
  * @property {string} brand
  * @property {string} model
@@ -32,32 +27,47 @@ function Person(name, age) {
  * @property {string} number
  * @property {Person} owner
  */
-function Car(brand, model, year, number, owner) {
+function Car(brand, model, year, number) {
     this.brand = brand;
     this.model = model;
     this.year = year;
     this.number = number;
-    this.owner = owner;
-    owner.cars.push(this)
-    this.outputInformationAboutCar = function (Car) {
-        if (owner.age > 18) {
-            console.log(brand, model, year, number, owner)
+    this.owner = null;
+    this.setOwner = function (person) {
+        if (person.age >= 18) {
+            this.owner = person
+        } else {
+            console.log(`The ${person.name}is under 18 years old`);
+            console.log('/-------------------------------------/')
         }
-        else if (owner.age < 18) {
-            console.log("The owner is under 18 years old")
-        }
-    }
+
+    };
+
+        this.outputInformationAboutCar = function (Car) {
+            console.log(`Brand:  ${brand}, Model: ${model}, Year: ${year}, Number: ${number}`);
+            if (this.owner !== null) {
+                console.log('Owner')
+                this.owner.outputInformationAboutPerson();
+            }
+        };
 }
 
 const vika = new Person('Vika', 29);
 const oleksii = new Person('Oleksii', 30)
 const kira = new Person('Kira', 16)
+const car1 = new Car('BMW', 'X6', 2020, 'AE3060AC')
+const car2 = new Car('KIA', 'Sportage', 2018, 'AE2032CA')
+const car3 = new Car('volkswagen', 'Jetta', 2015, 'AE0350AT')
+
 vika.outputInformationAboutPerson();
 oleksii.outputInformationAboutPerson();
 kira.outputInformationAboutPerson();
-const car1 = new Car('BMW', 'X6', 2020, 'AE3060AC', vika)
-const car2 = new Car('KIA', 'Sportage', 2018, 'AE2032CA', oleksii)
-const car3 = new Car('volkswagen', 'Jetta', 2015, 'AE0350AT', kira)
-car1.outputInformationAboutCar()
-car2.outputInformationAboutCar()
-car3.outputInformationAboutCar()
+
+car1.setOwner(vika);
+car2.setOwner(oleksii);
+car3.setOwner(kira);
+
+
+car1.outputInformationAboutCar();
+car2.outputInformationAboutCar();
+car3.outputInformationAboutCar();
